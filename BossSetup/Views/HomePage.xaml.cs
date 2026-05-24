e350e0cad4566150fe840049acb162b19c63a653
Daniel Batista <carvalho.daniel.15@gmail.com>
2026-05-23 23:50:34 -0300

    melhorando layout

--- BossSetup/Views/HomePage.xaml.cs
+++ BossSetup/Views/HomePage.xaml.cs
@@ -1,15 +1,43 @@
+using BossSetup.Models;
+using System.Collections.ObjectModel;
+
 namespace BossSetup.Views;
 
 public partial class HomePage : ContentPage
 {
+    public ObservableCollection<Produto> ProdutosDestaque { get; set; }
+
     public HomePage()
     {
         InitializeComponent();
 
-        // ?? Controle de acesso
+        ProdutosDestaque = new ObservableCollection<Produto>
+        (
+            App.ProdutoDB.Listar()
+            .Where(p => p.Destaque)
+        );
+
+        BindingContext = this;
+
+        // Controle de acesso
         if (App.UsuarioLogado?.Tipo != "Admin")
         {
             btnCadastrarProduto.IsVisible = false;
+        }
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        ProdutosDestaque.Clear();
+
+        var produtos = App.ProdutoDB.Listar()
+            .Where(p => p.Destaque);
+
+        foreach (var produto in produtos)
+        {
+            ProdutosDestaque.Add(produto);
         }
     }
 
